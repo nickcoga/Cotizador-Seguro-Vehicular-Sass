5551677d0702bed108a9b4047009707a8ce7c88b
Nick <nickcorreagarcia@gmail.com>
2022-09-26 13:36:17 -0500

    Update App.js with routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CarData from "./screens/car_data/CarData";
+import Home from "./screens/home/Home";
 
 function App() {
+  const [user, setUser] = useState();
+  const [plate, setPlate] = useState();
+  const [year, setYear] = useState(2019);
+  const [brand, setBrand] = useState("Wolkswagen");
+  const [enabledIndex, setEnabledIndex] = useState(1);
+
   return (
-    <div className="App">
-     <h1>Cotizador de Seguro Vehicular</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={<Home setUser={setUser} plate={plate} setPlate={setPlate} />}
+        />
+        <Route
+          path="/cardata/:id"
+          element={
+            <CarData
+              user={user}
+              year={year}
+              setYear={setYear}
+              brand={brand}
+              setBrand={setBrand}
+              enabledIndex={enabledIndex}
+              setEnabledIndex={setEnabledIndex}
+            />
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
