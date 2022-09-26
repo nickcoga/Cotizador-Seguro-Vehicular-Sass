174e085eef05993d26626864d264be9f25074bed
Nick <nickcorreagarcia@gmail.com>
2022-09-26 13:41:38 -0500

    create component inputTextSelect.js with styles in scss

---
+++ src/shared/inputTextSelect/InputTextSelect.js
@@ -0,0 +1,51 @@
+import classes from "./InputTextSelect.module.scss";
+
+export default function InputTextSelect({
+  required,
+  placeholderInput = "",
+  placeholderSelect = "",
+  nameInput = "",
+  nameSelect = "",
+  valueInput,
+  valueSelect,
+  showDefaultOption = false,
+  options = [],
+  onChangeInput,
+  onChangeSelect,
+}) {
+  return (
+    <div className={classes.container}>
+      <div className={classes.container__contentSelect}>
+        <select
+          className={classes.container__styledSelect}
+          type="select"
+          value={valueSelect}
+          name={nameSelect}
+          onChange={onChangeSelect}
+        >
+          {showDefaultOption ? (
+            <option disabled value="">
+              {placeholderSelect}
+            </option>
+          ) : null}
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.text}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className={classes.container__contentInputText}>
+        <input
+          className={classes.container__contentInputText__styledInputText}
+          value={valueInput}
+          name={nameInput}
+          onChange={onChangeInput}
+          required={required}
+          placeholder={placeholderInput}
+        />
+      </div>
+    </div>
+  );
+}
