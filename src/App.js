f9d3c428c7e78ae56f4412a6efd6f2f9d72520d6
Nick <nickcorreagarcia@gmail.com>
2022-09-27 17:59:19 -0500

    Add new routes buildplan and thanks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BuildPlan from "./screens/build_plan/BuildPlan";
 import CarData from "./screens/car_data/CarData";
 import Home from "./screens/home/Home";
+import Thanks from "./screens/thanks/Thanks";
 
 function App() {
   const [user, setUser] = useState();
@@ -18,6 +20,7 @@ function App() {
           path="/"
           element={<Home setUser={setUser} plate={plate} setPlate={setPlate} />}
         />
+
         <Route
           path="/cardata/:id"
           element={
@@ -32,6 +35,21 @@ function App() {
             />
           }
         />
+
+        <Route
+          path="/buildplan"
+          element={
+            <BuildPlan
+              plate={plate}
+              year={year}
+              brand={brand}
+              enabledIndex={enabledIndex}
+              setEnabledIndex={setEnabledIndex}
+            />
+          }
+        />
+
+        <Route path="/thanks" element={<Thanks user={user} />} />
       </Routes>
     </Router>
   );
