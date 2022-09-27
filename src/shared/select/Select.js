b21ff8f2c980c1f27e306b12df9f99b3a244ba2d
Nick <nickcorreagarcia@gmail.com>
2022-09-27 18:16:01 -0500

    cretate select component  with styles in scss

---
+++ src/shared/select/Select.js
@@ -0,0 +1,44 @@
+import classes from "./Select.module.scss";
+
+export default function Select({
+  label = "",
+  icon,
+  error = false,
+  showDefaultOption = false,
+  placeholder = "",
+  name = "",
+  value,
+  options = [],
+  onChange,
+}) {
+  return (
+    <div className={classes.container} error={error}>
+      {label && (
+        <label className={classes.container__inputLabel} htmlFor={name}>
+          {label}
+        </label>
+      )}
+      <select
+        className={classes.container__styledSelect}
+        type="select"
+        value={value}
+        name={name}
+        placeholder={placeholder}
+        id={name}
+        onChange={onChange}
+      >
+        {showDefaultOption ? (
+          <option disabled value="">
+            {placeholder}
+          </option>
+        ) : null}
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.text}
+          </option>
+        ))}
+      </select>
+      {icon}
+    </div>
+  );
+}
