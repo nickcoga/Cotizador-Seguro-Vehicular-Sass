8c61285bc4ff800f6430e9e27aaa03b83a1d5b87
Nick <nickcorreagarcia@gmail.com>
2022-09-28 14:12:35 -0500

    update styles with Grid in BuildPlan component

--- src/screens/build_plan/BuildPlan.js
+++ src/screens/build_plan/BuildPlan.js
@@ -1,10 +1,11 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import ButtonReturn from "../../shared/buttonReturn/ButtonReturn";
 import Header from "../../shared/header/Header";
 import Stepper from "../../shared/stepper/Stepper";
 import ContentPrice from "./components/ContentPrice";
 import Coverage from "./components/Coverage";
 import PlanCoverage from "./components/PlanCoverage";
+import classes from "./BuildPlan.module.scss";
 
 export default function BuildPlan({
   plate,
@@ -18,23 +19,25 @@ export default function BuildPlan({
   const [toggle3, setToggle3] = useState(false);
 
   return (
-    <Fragment>
+    <div className={classes.buildPlan}>
       <Header />
       <Stepper enabledIndex={enabledIndex} setEnabledIndex={setEnabledIndex} />
       <ButtonReturn
         enabledIndex={enabledIndex}
         setEnabledIndex={setEnabledIndex}
       />
-      <Coverage plate={plate} year={year} brand={brand} />
-      <PlanCoverage
-        toggle1={toggle1}
-        toggle2={toggle2}
-        toggle3={toggle3}
-        setToggle1={setToggle1}
-        setToggle2={setToggle2}
-        setToggle3={setToggle3}
-      />
+      <div className={classes.buildPlan__content}>
+        <Coverage plate={plate} year={year} brand={brand} />
+        <PlanCoverage
+          toggle1={toggle1}
+          toggle2={toggle2}
+          toggle3={toggle3}
+          setToggle1={setToggle1}
+          setToggle2={setToggle2}
+          setToggle3={setToggle3}
+        />
+      </div>
       <ContentPrice toggle1={toggle1} toggle2={toggle2} toggle3={toggle3} />
-    </Fragment>
+    </div>
   );
 }
