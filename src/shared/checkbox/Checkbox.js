853dbe6ee1f40652287564d37d0c374a1905653e
Nick <nickcorreagarcia@gmail.com>
2022-09-27 19:55:59 -0500

    cretate Checkbox component  with styles in scss

---
+++ src/shared/checkbox/Checkbox.js
@@ -0,0 +1,15 @@
+import classes from "./Checkbox.module.scss";
+
+export default function Checkbox({ text }) {
+  return (
+    <div className={classes.content}>
+      <input
+        className={classes.content__styledCheckbox}
+        id="styled-checkbox-1"
+        type="checkbox"
+        value="value1"
+      />
+      <label for="styled-checkbox-1">{text}</label>
+    </div>
+  );
+}
